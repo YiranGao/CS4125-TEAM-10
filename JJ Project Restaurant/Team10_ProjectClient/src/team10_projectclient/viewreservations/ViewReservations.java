5cd39de71b945200569475e335402e6fe68f2ee9
YIRAN GAO <43918870+YiranGao@users.noreply.github.com>
2018-11-16 23:49:54 +0000

    1

--- JJ Project Restaurant/Team10_ProjectClient/src/team10_projectclient/viewreservations/ViewReservations.java
+++ JJ Project Restaurant/Team10_ProjectClient/src/team10_projectclient/viewreservations/ViewReservations.java
@@ -12,6 +12,7 @@ package team10_projectclient.viewreservations;
  * */
 
 
+import team10_projectclient.view.MyTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
