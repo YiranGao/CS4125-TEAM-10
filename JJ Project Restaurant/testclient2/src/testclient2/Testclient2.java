a528060bd4e927284ed7b8438fe6c0472a1f1079
YIRAN GAO <43918870+YiranGao@users.noreply.github.com>
2018-11-17 00:32:32 +0000

    remove test files

--- JJ Project Restaurant/testclient2/src/testclient2/Testclient2.java
+++ JJ Project Restaurant/testclient2/src/testclient2/Testclient2.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication8;
+package testclient2;
 
 /**
  *
  * @author admin
  */
-public class JavaApplication8 {
+public class Testclient2 {
 
     /**
      * @param args the command line arguments
