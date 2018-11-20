8525c21afd6581428bab5370ed1fb99e4c966573
GRandles16 <gtarandles@gmail.com>
2018-11-20 12:49:41 +0000

    Latest DB revision
    Should be good

--- simplication/booking_system2.sql
+++ simplication/booking_system2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2018 at 09:12 PM
+-- Generation Time: Nov 20, 2018 at 01:38 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -36,6 +36,13 @@ CREATE TABLE `creditcards` (
   `card_number` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `creditcards`
+--
+
+INSERT INTO `creditcards` (`card_id`, `holder_name`, `validdate_year`, `validdate_month`, `card_number`) VALUES
+(1, 'Gavin V Randles', 11, 20, '6483749573638493');
+
 -- --------------------------------------------------------
 
 --
@@ -52,9 +59,16 @@ CREATE TABLE `customers` (
   `email` varchar(60) NOT NULL,
   `birthday` varchar(10) NOT NULL,
   `creditcard_id` int(11) DEFAULT NULL,
-  `customer_gender` tinyint(1) NOT NULL,
   `loyalty_points` int(3) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `username`, `password`, `firstname`, `lastname`, `phone_number`, `email`, `birthday`, `creditcard_id`, `loyalty_points`) VALUES
+(1, 'grand1', 'gav123', 'Gavin', 'Randles', '0852725054', 'gtarandles@gmail.com', '05/13/2018', 1, 0),
+;
 
 -- --------------------------------------------------------
 
@@ -69,6 +83,13 @@ CREATE TABLE `feedback` (
   `reservation_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedback_id`, `staff_id`, `feedback_mark`, `reservation_id`) VALUES
+(1, 1, 5, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -82,13 +103,20 @@ CREATE TABLE `reservations` (
   `bookingdate` datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   `table_id` int(11) DEFAULT NULL,
   `restaurant_id` int(11) NOT NULL,
-  `gluten_allergy` tinyint(1) NOT NULL,
-  `dairy_allergy` tinyint(1) NOT NULL,
-  `fish_allergy` tinyint(1) NOT NULL,
-  `shellfish_allergy` tinyint(1) NOT NULL,
-  `peanuts_allergy` tinyint(1) NOT NULL,
-  `soya_allergy` tinyint(1) NOT NULL
+  `gluten_allergy` tinyint(1) DEFAULT '0',
+  `dairy_allergy` tinyint(1) DEFAULT '0',
+  `fish_allergy` tinyint(1) DEFAULT '0',
+  `shellfish_allergy` tinyint(1) DEFAULT '0',
+  `peanuts_allergy` tinyint(1) DEFAULT '0',
+  `soya_allergy` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`reservation_id`, `customer_id`, `numofguests`, `bookingdate`, `table_id`, `restaurant_id`, `gluten_allergy`, `dairy_allergy`, `fish_allergy`, `shellfish_allergy`, `peanuts_allergy`, `soya_allergy`) VALUES
+(1, 1, 4, '2000-01-01 00:00:00', 1, 1, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -103,6 +131,13 @@ CREATE TABLE `restaurants` (
   `tableamount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`restaurant_id`, `name`, `location`, `tableamount`) VALUES
+(1, 'Team10 Limerick City', 'Limerick City, Limerick', 15);
+
 -- --------------------------------------------------------
 
 --
@@ -115,10 +150,19 @@ CREATE TABLE `staff` (
   `password` varchar(45) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `email` varchar(60) NOT NULL,
   `usertype` tinyint(11) NOT NULL,
   `feedback_mark` int(11) DEFAULT '0',
   `restaurant_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staff_id`, `username`, `password`, `firstname`, `surname`, `phone_number`, `email`, `usertype`, `feedback_mark`, `restaurant_id`) VALUES
+(1, 'grandles', 'gav123', 'Gavin', 'Randles', '0852725054', 'gtarandles@gmail.com', 2, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -132,6 +176,13 @@ CREATE TABLE `tables` (
   `table_state` tinyint(1) NOT NULL,
   `seat_amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`table_id`, `restaurant_id`, `table_state`, `seat_amount`) VALUES
+(1, 1, 0, 5);
 
 --
 -- Indexes for dumped tables
@@ -195,43 +246,43 @@ ALTER TABLE `tables`
 -- AUTO_INCREMENT for table `creditcards`
 --
 ALTER TABLE `creditcards`
-  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `restaurants`
 --
 ALTER TABLE `restaurants`
-  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tables`
 --
 ALTER TABLE `tables`
-  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
