3df4682b49f2ff2146e0d7fbdc306bf766348aa3
SeanW22 <seansimonwright@gmail.com>
2018-12-06 08:28:18 +0000

    Revert "test cases"
    This reverts commit 8b2577be8960a76a98787c1d97986d3499a14ba3.

--- simplication/Team10_Project2.0/test/controller/deleteCustomerControllerTest.java
+++ simplication/Team10_Project2.0/test/controller/deleteCustomerControllerTest.java
@@ -1,48 +1,48 @@
-//package controller;
-//
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// *
-// * @author mjs-laptop
-// */
-//public class deleteCustomerControllerTest {
-//    
-//    public deleteCustomerControllerTest() {
-//    }
-//    
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//    
-//    @Before
-//    public void setUp() {
-//    }
-//    
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of deleteCustomer method, of class deleteCustomerController.
-//     */
-//    @org.junit.Test
-//    public void testDeleteCustomer() {
-//        System.out.println("deleteCustomer");
-//        String userName = "";
-//        deleteCustomerController instance = new deleteCustomerController();
-//        instance.deleteCustomer(userName);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//    
-//}
+package controller;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mjs-laptop
+ */
+public class deleteCustomerControllerTest {
+    
+    public deleteCustomerControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of deleteCustomer method, of class deleteCustomerController.
+     */
+    @org.junit.Test
+    public void testDeleteCustomer() {
+        System.out.println("deleteCustomer");
+        String userName = "";
+        deleteCustomerController instance = new deleteCustomerController();
+        instance.deleteCustomer(userName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
